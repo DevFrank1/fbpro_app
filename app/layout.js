57f0e183ac2f95a11a8579f0a6bbf7498fffd8c0
DevFrank1 <fbaylarzade11@gmail.com>
2023-04-24 22:37:22 +0400

    initialization

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Footer from './Footer'
+import Navbar from './Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <main className='container'>{children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }
