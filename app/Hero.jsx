9d8d64c062264ab51ef374bcdece0656d62dba31
DevFrank1 <fbaylarzade11@gmail.com>
2023-04-27 15:57:38 +0400

    initialization 0.0101

--- app/Hero.jsx
+++ app/Hero.jsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
-import { createStyles, Container, Text, Button, Group, rem } from '@mantine/core';
+import { createStyles, Container, Text, Button, Group, rem, Avatar } from '@mantine/core';
 import { GithubIcon } from '@mantine/ds';
 import { TypeAnimation } from "react-type-animation";
+import Image from "next/image";
+
+import NextImage from '../public/next.svg';
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
     position: 'relative',
     boxSizing: 'border-box',
     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '0 6rem',
   },
 
   inner: {
     position: 'relative',
-    paddingTop: rem(70),
-    paddingBottom: rem(70),
+    paddingTop: rem(96),
+    paddingBottom: rem(96),
 
     [theme.fn.smallerThan('sm')]: {
       paddingBottom: rem(80),
@@ -72,7 +80,7 @@ export function Hero() {
 
   return (
     <div className={classes.wrapper}>
-      <Container size={700} className={classes.inner}>
+      <Container size={750} className={classes.inner} sx={{margin: 0}}>
         <h1 className={classes.title}>
           Hi<span style={{ color: 'orange' }}>,</span> I<span style={{ color: 'orange' }}>'</span>m
           <br />
@@ -114,6 +122,7 @@ export function Hero() {
           </Button>
         </Group>
       </Container>
+      <Avatar  radius={70} size={400} src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sam-worthington-avatar-the-way-of-the-water-1670323169.jpg?crop=0.528xw:1.00xh;0.134xw,0&resize=1200:*'/>
     </div>
   );
 }
--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,58 @@
 "use client"
 import ProjectCard from '../projects/ProjectCard'
+import { createStyles, Paper, Text, ThemeIcon, rem } from '@mantine/core';
+import { IconColorSwatch } from '@tabler/icons-react';
 
-export default function About() {
+import './page.css';
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    position: 'relative',
+    cursor: 'pointer',
+    overflow: 'hidden',
+    transition: 'transform 150ms ease, box-shadow 100ms ease',
+    padding: theme.spacing.xl,
+    paddingLeft: `calc(${theme.spacing.xl} * 2)`,
+
+    '&:hover': {
+      boxShadow: theme.shadows.md,
+      transform: 'scale(1.02)',
+    },
+
+    '&::before': {
+      content: '""',
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      width: rem(6),
+      backgroundImage: theme.fn.linearGradient(0, theme.colors.pink[6], theme.colors.orange[6]),
+    },
+
+    margin: '6rem 4rem'
+  },
+}));
+
+export default function About({ title, description }) {
+  const { classes } = useStyles();
   return (
-    <div>
-     about
+    <div className='about'>
+      <Paper withBorder radius="md" className={classes.card}>
+        <ThemeIcon
+          size="xl"
+          radius="md"
+          variant="gradient"
+          gradient={{ deg: 0, from: 'pink', to: 'orange' }}
+        >
+          <IconColorSwatch size={rem(28)} stroke={1.5} />
+        </ThemeIcon>
+        <Text size="xl" weight={500} mt="md">
+          Theming documentation
+        </Text>
+        <Text size="sm" mt="sm" color="dimmed">
+          Extend default theme with any amount of additional colors, replace shadows, radius, spacing, fonts and many other properties to match your design requirements. Mantine theme is just an object, you can subscribe to it in any part of application via context and use it to build your own components.
+        </Text>
+      </Paper>
     </div>
   )
 }
